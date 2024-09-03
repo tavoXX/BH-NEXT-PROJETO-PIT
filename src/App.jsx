5195f05e7056de649eb19872b53654e6e702ae6f
Gustavo-Bh <gustavoalves0706@gmail.com>
2024-09-03 19:55:04 -0300

    Deletando

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/home'
+
 
 function App() {
 
